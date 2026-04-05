042f58ea3472a86c473538717646f8a9f4ef1bac
ElKriZ <pazochris@gmail.com>
2026-04-05 01:33:33 -0500

    Animacion video

--- src/barber/pages/AboutMe.tsx
+++ src/barber/pages/AboutMe.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 
 const images = [
     "me/me1.jpeg",
@@ -8,24 +8,38 @@ const images = [
     "me/me4.jpeg",
 ]
 
-/* const videos = [
-    "me/video1.mp4",
-    "me/video2.mp4",
-] */
+const videos = [
+    "me/video1.webm",
+    "me/video2.webm",
+    "me/video3.webm",
+    "me/video4.webm",
+]
 
 const AboutMe = () => {
 
-    const [index, setIndex] = useState(0)
+    const [indexImg, setIndexImg] = useState(0)
+    const [indexVideo, setIndexVideo] = useState(0)
     const [aboutme, setAboutMe] = useState('hidden')
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setIndex((prev) => (prev + 1) % images.length)
-        }, 5000)
+        const intervalImg = setInterval(() => {
+            setIndexImg((prev) => (prev + 1 === images.length ? 0 : prev + 1))
+        }, 3000)
 
-        return () => clearInterval(interval)
+        return () => clearInterval(intervalImg)
     }, [images.length])
 
+    const nextVideo = () => {
+        setIndexVideo((prev) =>
+            prev === videos.length - 1 ? 0 : prev + 1
+        )
+    }
+
+    const prevVideo = () => {
+        setIndexVideo((prev) =>
+            prev === 0 ? videos.length - 1 : prev - 1
+        )
+    }
 
     return (
         <section id="aboutMe" className="bg-[#0B0B0B] p-6 text-white scroll-mt-30">
@@ -55,8 +69,8 @@ const AboutMe = () => {
                             className="absolute w-full h-200 md:h-270 object-cover"
                             initial={{ opacity: 0, scale: 1.1 }}
                             animate={{
-                                opacity: i === index ? 1 : 0,
-                                scale: i === index ? 1 : 1.05
+                                opacity: i === indexImg ? 1 : 0,
+                                scale: i === indexImg ? 1 : 1.05
                             }}
                             transition={{ duration: 0.8 }}
                         />
@@ -71,7 +85,7 @@ const AboutMe = () => {
                 {/* Content */}
                 <div className="p-10 flex flex-col justify-center gap-4 ">
 
-                    
+
                     <span className={`${aboutme} text-gray-300 leading-relaxed font-main text-md lg:text-2xl `}>
                         Cuando te cortas el cabello conmigo, no solo estás recibiendo un servicio… estás viviendo una experiencia.
 
@@ -102,9 +116,49 @@ const AboutMe = () => {
             </div>
 
             {/* Videos */}
-            
-            
-            
+
+            <div className="mt-12 flex flex-col items-center gap-4">
+
+                <div className="relative w-100 md:w-120">
+
+                    <AnimatePresence mode="wait">
+                        <motion.video
+                            key={videos[indexVideo]}
+                            src={videos[indexVideo]}
+                            className="w-full rounded-2xl shadow-lg"
+                            controls
+                            controlsList="nodownload noremoteplayback"
+                            disablePictureInPicture
+                        />
+                    </AnimatePresence>
+
+                    {/* button left */}
+                    <button
+                        onClick={prevVideo}
+
+                        className=" cursor-pointer absolute top-1/2 left-2 -translate-y-1/2 bg-black/40 text-white px-4 py-2 rounded-full"
+                    >
+                        <img
+                            src="tijera.png"
+                            className="w-5 rotate-180 invert brightness-0"
+                            alt="<-" />
+                    </button>
+
+                    {/* button right */}
+                    <button
+                        onClick={nextVideo}
+                        className="cursor-pointer absolute top-1/2 right-2 -translate-y-1/2 bg-black/40 text-white px-4 py-2 rounded-full"
+                    >
+                        <img
+                            src="tijera.png"
+                            className="w-5 invert brightness-0"
+                            alt="->" />
+                    </button>
+
+                </div>
+            </div>
+
+
 
         </section>
     )
